49f91184d35ed1782b12ae92a75939d53e27e2cb
kevinsulatra <ksulatra@gmail.com>
2022-05-25 13:44:34 +0000

    Add vars and iam member resource

--- terraform/modules/service_account/main.tf
+++ terraform/modules/service_account/main.tf
@@ -2,3 +2,10 @@ resource "google_service_account" "sa" {
   account_id   = "minecraft"
   display_name = "minecraft"
 }
+
+resource "google_service_account_iam_member" "sa" {
+  service_account_id = google_service_account.sa.name
+  role               = "roles/owner"
+
+  member = "serviceAccount:$(google_service_account.sa.email)"
+}
---
+++ terraform/vars.tf
@@ -0,0 +1,5 @@
+variable "project" {
+  description = "Google Project"
+  type        = string
+  default     = "diamond-62188"
+}
